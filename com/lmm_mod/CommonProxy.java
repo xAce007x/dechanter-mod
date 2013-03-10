6303f50f29253ecbf232934d5bfffb678bc331fa
xAce007x <acusick@charter.net>
2013-03-10 10:06:17 -0500

    Working Recipie/block

--- com/lmm_mod/CommonProxy.java
+++ com/lmm_mod/CommonProxy.java
@@ -1,34 +1,9 @@
 package lmm_mod;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
-import cpw.mods.fml.common.network.IGuiHandler;
-
-public class CommonProxy implements IGuiHandler {
-	public void registerRenderInformation() {
-
-	}
-
-	public void registerTileEntitySpecialRenderer(/* PLACEHOLDER */) {
-		// TODO: Add special render types here
-	}
-
-	@Override
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world,
-			int x, int y, int z) {
-		
-		return null;
+public class CommonProxy {
 	
+	public void registerRenderers() {
+		//Server sided stuff!!
 	}
-
-	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world,
-			int x, int y, int z) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public World getClientWorld() {
-		return null;
-	}
+	
 }
--- com/lmm_mod/LetsMod_Mod.java
+++ com/lmm_mod/LetsMod_Mod.java
@@ -1,10 +1,16 @@
 package lmm_mod;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = "LetsMod_Mod", name = "LetsMod Mod", version = "In-Dev 1.0")
 @NetworkMod(
@@ -14,6 +20,11 @@ import cpw.mods.fml.common.network.NetworkMod;
 		packetHandler = PacketHandler.class )
 
 public class LetsMod_Mod {
+	
+	
+	// Blocks
+	public final static Block dechanterBlock = new BlockDechanter(1000, 1, Material.ground);
+	
 	@Instance
 	public static LetsMod_Mod instance;
 
@@ -21,7 +32,23 @@ public class LetsMod_Mod {
 	public static CommonProxy proxy;
 	
 	@Init
-	public void load() {
+	public void load(FMLInitializationEvent event) {
+		proxy.registerRenderers();
+		
+		// ItemStacks
+		ItemStack dirtStack = new ItemStack(Block.dirt);
+		ItemStack dechanterStack = new ItemStack(dechanterBlock);
+		// ItemStack diamondStack = new ItemStack(Item.diamond, 64);
+		
+		// Blocks
+		GameRegistry.registerBlock(dechanterBlock, "Ace_Dechanter");
+		
+		// Language
+		LanguageRegistry.addName(dechanterBlock, "Dechanter");
+		
+		// Recipes
+		// GameRegistry.addShapelessRecipe(diamondStack, dirtStack);
+		GameRegistry.addShapelessRecipe(dechanterStack, dirtStack);
 
 	}
 	
